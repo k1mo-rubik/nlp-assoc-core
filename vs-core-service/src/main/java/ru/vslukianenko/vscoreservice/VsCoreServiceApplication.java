c51ed023baf97edc64563be3ec0bb09ac129a5e4
k1mo rub1k <vitaliy0422@gmail.com>
2024-06-23 14:07:51 +0300

    initial project commit

---
+++ vs-core-service/src/main/java/ru/vslukianenko/vscoreservice/VsCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.vslukianenko.vscoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsCoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsCoreServiceApplication.class, args);
+    }
+
+}
---
+++ vs-core-service/src/test/java/ru/vslukianenko/vscoreservice/VsCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vslukianenko.vscoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsCoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
